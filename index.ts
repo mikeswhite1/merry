232205b09be399c3d610cf44f8407cd4d2739ed1
mikeswhite1 <mike.s.white1@gmail.com>
2019-08-14 09:44:42 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
 // Import stylesheets
 import './style.css';
-import * as functions from 'firebase-functions';
+// import * as functions from 'firebase-functions';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript HOOO Starter</h1>`;
+appDiv.innerHTML = `<h1>TypeScript HOOOYYYY Starter</h1>`;
 
