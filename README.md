c30148e32501ea1867a594ee249d038dd143cdce
mikeswhite1 <mike.s.white1@gmail.com>
2019-08-14 09:36:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-nnzkuu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-nnzkuu)
---
+++ index.ts
@@ -0,0 +1,15 @@
+// Import stylesheets
+import './style.css';
+import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript HOOO Starter</h1>`;
+
