8cef6c423ad7e2a1a863d892b4a9d5a4f7f50348
mikeswhite1 <mike.s.white1@gmail.com>
2019-08-14 14:34:30 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -28,14 +28,10 @@ var firebaseConfig = {
 
 var db = firebase.firestore();
 
-db.collection("history").add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
+const history = db.collection("history");
+history.doc("last").set({
+  walker: "Chris",
+  time: "11:11:00 AM"
 });
+
+
