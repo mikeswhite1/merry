c1f92e9377d989520a53d8474c2a0b6c53b0c891
mikeswhite1 <mike.s.white1@gmail.com>
2019-08-15 12:46:55 -0500

    3 files modified

--- handler.js
+++ handler.js
@@ -1,3 +1,14 @@
 /// <reference path="angular.js" />
 
 var app = angular.module("myModule", [])
+
+
+$(function (){
+  $.ajax({
+    type: 'GET',
+    url: 'https://merry-972e0.firebaseapp.com',
+    success: function(data) {
+      console.log('success', data)
+    }
+  })
+});
